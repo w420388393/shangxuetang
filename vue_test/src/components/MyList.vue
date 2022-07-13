3ba94093478f97e28fc14c7ba4eef49277404ce0
w420388393 <420388393@qq.com>
2022-07-13 16:16:01 +0800

    20220713

---
+++ vue_test/src/components/MyList.vue
@@ -0,0 +1,41 @@
+<template> 
+  <ul class="todo-main">
+    <MyItem/>
+  </ul>
+</template>
+
+<script>
+  import MyItem from './MyItem'
+  const todoOptions = ["打代码","睡觉","吃饭"]
+  export default {
+    name:'MyList',
+    components:{
+      MyItem
+    },
+    data(){
+      return {
+        todos:todoOptions
+      }
+    }
+    
+  }
+</script>
+<style scoped>
+    /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
+  
