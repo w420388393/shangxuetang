97f8bb7262317e2deaf66e491ec61247046f8abd
w420388393 <420388393@qq.com>
2022-07-29 16:48:51 +0800

    20220729

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -2,13 +2,14 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-import plugins from './plugins'
 //关闭Vue的生产提示
-Vue.config.productionTip =false
-Vue.use(plugins)
+Vue.config.productionTip = false
 
 //创建vm
 new Vue({
-  el:"#app",
-  render:h=>h(App)
+	el:'#app',
+	render: h => h(App),
+	beforeCreate() {
+		Vue.prototype.$bus = this
+	},
 })
--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,5 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  //开启代理服务器(方式一)
+  /* devServer:{
+    proxy:'http://localhost:5000'
+  }, */
+   //开启代理服务器(方式二)
+   devServer: {
+    proxy: {
+      '/hehe': {
+        target: 'http://localhost:5000',
+        pathRewrite:{"^/hehe":""},
+        //ws: true,
+        //changeOrigin: true
+      },
+      
+    }
+  }
 })
